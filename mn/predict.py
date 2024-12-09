ad2019561b34e4eb979547507505f7f0b39ce3eb
ligang1841 <ligang1840@gmail.com>
2024-12-09 14:42:59 +0800

    ..

--- mn/predict.py
+++ mn/predict.py
@@ -1,5 +1,6 @@
 from ultralytics import YOLO
 import cv2
+import os
 from glob import glob
 import argparse
 
@@ -12,37 +13,71 @@ def predict(chosen_model, img, classes=[], conf=0.5):
     return results
 
 
-def predict_and_detect(chosen_model, img, classes=[], conf=0.5, rectangle_thickness=2, text_thickness=1):
+def predict_and_detect(chosen_model, img, classes=[], conf=0.5, 
+                       rectangle_thickness=2, text_thickness=1):
     results = predict(chosen_model, img, classes, conf=conf)
     for result in results:
         for box in result.boxes:
-            cv2.rectangle(img, (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
-                          (int(box.xyxy[0][2]), int(box.xyxy[0][3])), (255, 0, 0), rectangle_thickness)
+            cv2.rectangle(img, 
+                    (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
+                    (int(box.xyxy[0][2]), int(box.xyxy[0][3])),
+                    (255, 0, 0),
+                    rectangle_thickness)
             cv2.putText(img, f"{result.names[int(box.cls[0])]}",
                         (int(box.xyxy[0][0]), int(box.xyxy[0][1]) - 10),
-                        cv2.FONT_HERSHEY_PLAIN, 1, (255, 0, 0), text_thickness)
+                        cv2.FONT_HERSHEY_PLAIN, 1,
+                        (255, 0, 0), text_thickness)
     return img, results
 
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='mn predict')
-    parser.add_argument('--model', '-m',default='weights/last.pt')
-    parser.add_argument('--image', '-img')
-    parser.add_argument('--display', '-d', action='store_true')
-    parser.add_argument('--save', '-s')
-    parser.add_argument('--score', '-sc',default=0.2, type=float)
-
+    parser.add_argument('--model',  '-m', default='weights/last.pt')
+    parser.add_argument('--val_dir','-v', default='val')
+    parser.add_argument('--image',  '-i', default='')
+    parser.add_argument('--display','-d', action='store_true')
+    parser.add_argument('--save',   '-s', default='/dev/shm',
+                        help="saved name or dir")
+    parser.add_argument('--limit',  '-l', default=10, type=int)
+    parser.add_argument('--score', '-sc', default=0.2, type=float)
     args = parser.parse_args()
 
     model = YOLO(args.model)
 
     # read the image
-    image = cv2.imread(args.image)
-    result_img, _ = predict_and_detect(model, image, classes=[], conf=args.score)
+    if os.path.isdir(args.val_dir):
+        print('test val')
+        os.makedirs(f"{args.save}/mctest",exist_ok=True)
+        count = 0
+        imgs = glob(args.val_dir + "/*.png")
+        for img in imgs:
+            image = cv2.imread(img)
+            result_img, _ = predict_and_detect(model,
+                            image,
+                            classes=[],
+                            conf=args.score)
+            
+            bname = os.path.basename(img)
+            cv2.imwrite(f"{args.save}/mctest/{bname}", result_img)
+            print('predicted',bname)
+            
+            if count>=args.limit: break
+            count += 1
 
-    if args.save is not None:
-        cv2.imwrite(args.save, result_img)
-    if args.display:
-        cv2.imshow("Image", result_img)
-        cv2.waitKey(0)
+    elif os.path.isfile(args.image):
+        print(args.image)
+        image = cv2.imread(args.image)
+        result_img, _ = predict_and_detect(model,
+                            image,
+                            classes=[],
+                            conf=args.score)
+        bname = os.path.basename(args.image)
+        if args.save is not None:
+            cv2.imwrite(f"{args.save}/{bname}", result_img)
+            print('predicted',bname)
+        if args.display:
+            cv2.imshow("Image", result_img)
+            cv2.waitKey(0)
+    else:
+        print('do nothing')
--- mn/train.py
+++ mn/train.py
@@ -1,30 +1,32 @@
 #import warnings
 #warnings.filterwarnings('ignore')
 from ultralytics import YOLO
-import sys
+import os,sys
 import argparse
 from glob import glob
 
 wname = 'exp'
+model_dir = 'runs/train'
 
 def get_last_exp():
-    dirs = os.listdir('runs/train')
+    dirs = os.listdir(model_dir)
     num = []
     for i in dirs:
+        if i == wname:
+            continue
         n = int(i[len(wname):])
-        num.append(n)
-    if num == []:
-        return -1
-    else:
+        if os.path.isfile(f"{model_dir}/{i}/weights/last.pt"):
+            num.append(n)
+    if num != []:
         num = sorted(num)
         return num[-1]
-
+    return -1
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='ultralytics-v11 train')
     parser.add_argument('--imgsz', '-sz', default=1360, type=int)
     parser.add_argument('--epochs', '-e', default=500, type=int)
-    parser.add_argument('--resume', '-c', action='store_true')
+    parser.add_argument('--resume', '-c', default=0, type=int,help="10 e.g.")
     args = parser.parse_args()
 
     # 如何切换模型版本, 上面的ymal文件可以改为 yolov11s.yaml就是使用的v11s,
@@ -32,14 +34,11 @@ if __name__ == '__main__':
     #   那么如果想使用其它版本就把上面的名称改为yolov11l-XXX.yaml即可
     #   （改的是上面YOLO中间的名字不是配置文件的）！
 
-    if args.resume:    
-        initpt = "yolo11.yaml"
+    if args.resume>0:
+        initpt = f"runs/train/{wname}{args.resume}/weights/last.pt"
     else: # resumen train
-        last_num = get_last_exp()
-        if last_num >=0:
-            initpt = f"runs/train/exp{last_num}/weights/last.pt"
-        else:
-            initpt = "yolo11.yaml"
+        initpt = "yolo11.yaml"
+
     model = YOLO(initpt)
     
     # 是否加载预训练权重,科研不建议大家加载否则很难提升精度
@@ -64,7 +63,7 @@ if __name__ == '__main__':
                 # 训练了200轮次是没有办法进行续训的.
                 resume  = args.resume, 
                 amp     = True,  # 如果出现训练损失为Nan可以关闭amp
-                project = 'runs/train',
+                project = model_dir,
                 name    = wname,
                 )
  
