73959eac20e43353ed88be38272bb95b115ea3f8
ligang1841 <ligang1840@gmail.com>
2025-02-08 12:13:16 +0800

    ..

--- pbc/predict.py
+++ pbc/predict.py
@@ -17,8 +17,6 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5,
                        rectangle_thickness=2, text_thickness=1):
     results = predict(chosen_model, img, classes, conf=conf)
     for result in results:
-        print(result)
-        exit()
         for box in result.boxes:
             cv2.rectangle(img, 
                     (int(box.xyxy[0][0]), int(box.xyxy[0][1])),
@@ -35,12 +33,11 @@ def predict_and_detect(chosen_model, img, classes=[], conf=0.5,
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='mn predict')
     parser.add_argument('--model',  '-m', default='weights/last.pt')
-    parser.add_argument('--val_dir','-v', default='val')
-    parser.add_argument('--image',  '-i', default='')
+    parser.add_argument('--val_dir','-v', default='val',help="val image dir")
+    parser.add_argument('--image',  '-i', default='',help="one image")
     parser.add_argument('--display','-d', action='store_true')
-    parser.add_argument('--save',   '-s', default='/dev/shm',
-                        help="saved name or dir")
-    parser.add_argument('--limit',  '-l', default=100, type=int)
+    parser.add_argument('--save',   '-s', default='/dev/shm',help="saved name or dir")
+    parser.add_argument('--limit',  '-l', default=30, type=int)
     parser.add_argument('--score', '-sc', default=0.5, type=float)
     args = parser.parse_args()
 
@@ -48,10 +45,12 @@ if __name__ == "__main__":
 
     # read the val's images
     if os.path.isdir(args.val_dir):
-        print('test val')
-        os.makedirs(f"{args.save}/mctest",exist_ok=True)
+        print(f'test save to {args.val_dir}')
+        os.makedirs(f"{args.save}/pbctest",exist_ok=True)
         count = 0
         imgs = glob(args.val_dir + "/*.png")
+        if imgs == []:
+            imgs = glob(args.val_dir + "/*.jpg")
         for img in imgs:
             image = cv2.imread(img)
             result_img, _ = predict_and_detect(model,
@@ -60,14 +59,14 @@ if __name__ == "__main__":
                             conf=args.score)
             
             bname = os.path.basename(img)
-            cv2.imwrite(f"{args.save}/mctest/{bname}", result_img)
+            cv2.imwrite(f"{args.save}/pbctest/{bname}", result_img)
             print('predicted',bname)
             
             if count>=args.limit: break
             count += 1
     # read one image
     elif os.path.isfile(args.image):
-        print(args.image)
+        print('test',args.image)
         image = cv2.imread(args.image)
         result_img, _ = predict_and_detect(model,
                             image,
--- pbc/train.py
+++ pbc/train.py
@@ -5,17 +5,22 @@ import os,sys
 import argparse
 from glob import glob
 
-wname = 'exp'
-model_dir = 'runs/train'
+WNAME = 'exp'
+MODEL_DIR = 'runs/train'
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='pbc-nv11 train')
     parser.add_argument('--yaml',  '-y', default='mot_wbc2')
-    parser.add_argument('--imgsz',  '-sz', default=2560, type=int)
-    parser.add_argument('--epochs', '-i', default=10000, type=int)
-    parser.add_argument('--model',  '-m', default='yolo11.yaml',help="base model")
-    parser.add_argument('--resume', '-c', action='store_true')
+    parser.add_argument('--imgsz',  '-sz', default=2560, type=int,help="img width")
+    parser.add_argument('--epochs', '-i', default=30, type=int,help="about img-count*1.5 or 2.5")
+    parser.add_argument('--model',  '-m', default='yolo11.yaml',help="base model: weights/yolo11n.yaml or weights/last.pt")
+    #parser.add_argument('--resume', '-c', action='store_true')
     args = parser.parse_args()
+
+    print("e.g. -y mot_wbc2 -i 10 -m weights/last.pt -c")
+    print("For continue: training by loading the weights from your previously trained model and starting a new training session without the resume parameter")
+    print("  model = YOLO('last.pt')")
+    print("  model.train(data='data.yaml', epochs=200, imgsz=640, batch=64, workers=0)")
 
     # 如何切换模型版本, 上面的ymal文件可以改为 yolov11s.yaml就是使用的v11s,
     # 类似某个改进的yaml文件名称为yolov11-XXX.yaml
@@ -37,16 +42,17 @@ if __name__ == '__main__':
                 single_cls=False,  # 是否是单类别检测
                 batch   = 2,
                 close_mosaic=0,
-                workers = 0,
+                workers = 2,
                 device  = '0',
                 optimizer= 'SGD', # using SGD 优化器 默认为auto建议大家使用固定的.
                 # 续训的话这里填写True, 
                 # yaml文件的地方改为exp(n)/weights/last.pt的地址,
                 # 需要注意的是如果你设置训练200轮次模型，
                 # 训练了200轮次是没有办法进行续训的.
-                resume  = args.resume, 
+                resume  = False, 
+                #pretrained=True,
                 amp     = True,  # 如果出现训练损失为Nan可以关闭amp
-                project = model_dir,
-                name    = wname,
+                project = MODEL_DIR,
+                name    = WNAME,
                 )
  
